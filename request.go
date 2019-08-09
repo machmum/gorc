157f7d285fff203852ccbe473d4105f28a42f960
machmum.alief <machmum.alief@transdigital.co.id>
2019-08-09 10:48:29 +0700

    add requestID

---
+++ request.go
@@ -0,0 +1,40 @@
+package gorc
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"os"
+	"strings"
+	"sync/atomic"
+)
+
+// Ported from Goji's middleware, source:
+// https://github.com/zenazn/goji/tree/master/web/middlewar
+
+// var prefix string
+var reqid uint64
+
+// RequestID is a middleware that injects a request ID into the context of each
+// request. A request ID is a string of the form "host.example.com/random-0001",
+// where "random" is a base62 random string that uniquely identifies this go
+// process, and where the last number is an atomically incremented request
+// counter.
+func RequestID() string {
+	var prefix string
+
+	hostname, err := os.Hostname()
+	if hostname == "" || err != nil {
+		hostname = "localhost"
+	}
+	var buf [12]byte
+	var b64 string
+	for len(b64) < 10 {
+		rand.Read(buf[:])
+		b64 = base64.StdEncoding.EncodeToString(buf[:])
+		b64 = strings.NewReplacer("+", "", "/", "").Replace(b64)
+	}
+	prefix = Join(hostname, b64[0:10])
+
+	return fmt.Sprintf("%s-%06d", prefix, atomic.AddUint64(&reqid, 1))
+}
